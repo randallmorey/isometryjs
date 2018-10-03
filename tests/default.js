ebc23735616d3813c47c1ef75ad0030cfacdc1f3
Randall Morey <randall@randallmorey.com>
2018-10-03 10:32:30 -0400

    feat(radians): add toRadians utility function

--- tests/default.js
+++ tests/default.js
@@ -1,10 +1,16 @@
-import { foo } from '../index';
+import { toRadians } from '../index';
 import assert from 'assert';
 
 describe('isometryjs', () => {
-  describe('foo()', () => {
-    it('works', () => {
-      assert.equal(foo(1, 1), 2);
+  describe('radians', () => {
+    describe('toRadians()', () => {
+      it('works', () => {
+        assert.equal(toRadians(0), 0);
+        assert.equal(toRadians(45), 0.7853981633974483);
+        assert.equal(toRadians(90), 1.5707963267948966);
+        assert.equal(toRadians(180), Math.PI);
+        assert.equal(toRadians(360), 6.283185307179586);
+      });
     });
   });
 });
