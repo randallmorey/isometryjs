3068ef4a83c58c8bd392d5f9c4f7a9c30cb3f7a1
Randall Morey <randall@randallmorey.com>
2018-12-14 17:13:38 -0500

    feat(dotProduct): add dotProduct utility function

--- src/utils.js
+++ src/utils.js
@@ -16,4 +16,19 @@ function toDegrees (radians) {
   return radians / (Math.PI / 180);
 };
 
-export { toRadians, toDegrees };
+/**
+ * Calculates the dot product of two n-tuples (arrays of numbers).
+ *
+ *   [1, 2, 3] * [4, 5, 6] = (1 * 4) + (2 * 5) + (3 * 6)
+ *
+ * @param {Array[number]} tuple1
+ * @param {Array[number]} tuple2
+ * @return {number}
+ */
+function dotProduct (tuple1, tuple2) {
+  return tuple1.reduce(
+    (accumulator, value, i) => (value * tuple2[i]) + accumulator,
+    0);
+};
+
+export { toRadians, toDegrees, dotProduct };
